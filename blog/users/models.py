9189954501ff08847213c1817627f949de456254
sanjeevz3009@gmail.com <sanjeevz3009@gmail.com>
2021-08-16 22:19:44 +0100

    AWS S3 Done.

--- blog/users/models.py
+++ blog/users/models.py
@@ -11,12 +11,12 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    def save(self):
-        super().save()
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
         
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
         
-        if img.height > 300 or img.width > 300:
-            outputSize = (300, 300)
-            img.thumbnail(outputSize)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         outputSize = (300, 300)
+    #         img.thumbnail(outputSize)
+    #         img.save(self.image.path)
