b2e85b2a75508ff9b5e86fd355d95d307c60958a
sanjeevz3009@gmail.com <sanjeevz3009@gmail.com>
2021-08-16 02:57:03 +0100

    Email & Password Reset Done.

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -26,6 +26,16 @@ urlpatterns = [
     path('profile/', userViews.profile, name="profile"),
     path('login/', authViews.LoginView.as_view(template_name="users/login.html"), name="login"),
     path('logout/', authViews.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    
+    path('password-reset/', 
+        authViews.PasswordResetView.as_view(template_name="users/password_reset.html"), name="password_reset"),
+    path('password-reset/done/', 
+        authViews.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>/', 
+        authViews.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('password-reset-complete/', 
+        authViews.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name="password_reset_complete"),
+    
     path('', include('blogApp.urls')),
 ]
 
