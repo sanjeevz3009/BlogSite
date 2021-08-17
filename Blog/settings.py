3e39352c6da7d9bb474ff41ca429d96a13f07f0d
sanjeevz3009@gmail.com <sanjeevz3009@gmail.com>
2021-08-17 01:15:22 +0100

    Settings.py and requirements.txt updated.

--- Blog/settings.py
+++ Blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -159,3 +160,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
